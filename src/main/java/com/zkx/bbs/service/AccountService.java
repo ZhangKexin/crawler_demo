fc284fbd5cc7d1dd1f7b3bc06dadd12bba4b3e1b
ZhangKexin <zkexing@163.com>
2017-08-24 20:23:54 +0800

    first commit

--- src/main/java/com/zkx/bbs/service/AccountService.java
+++ src/main/java/com/zkx/bbs/service/AccountService.java
@@ -4,7 +4,6 @@ import com.zkx.bbs.dao.UserDao;
 import com.zkx.bbs.entity.User;
 import com.zkx.bbs.exception.BBSErrorNo;
 import com.zkx.bbs.exception.ErrorNoException;
-import com.zkx.bbs.util.PasswordHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +18,9 @@ public class AccountService {
     @Autowired
     private UserService userService;
 
+    /**
+     * 注册
+     */
     public Long register(String phone, String password) {
         User user = userDao.queryUserByPhone(phone);
         if (user != null) {
@@ -27,8 +29,6 @@ public class AccountService {
         Long userId = userDao.insertUser(phone, password);
         return userId;
     }
-
-
 
 
 }
